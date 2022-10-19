ab4fe8dc43275881091f43c51a9091f190bb3ca7
TDiaz83 <tomas.enrique.diaz@gmail.com>
2022-10-19 15:28:03 -0500

    Got posting to work

--- routes/notes.js
+++ routes/notes.js
@@ -7,4 +7,23 @@ notes.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.post('/notes', (req, res) => {
+    console.log(`${req.method} received for notes.`)
+    console.log(req.body);
+    
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        }
+
+        readAndAppend(newNote, './db/db.json')
+        res.json('Note added succesfully!')
+    } else {
+        res.error('Cannot add note.')
+    }
+})
+
 module.exports = notes;
